f06842dfe603cd0b18a7969dcb7b205afd3a5e9e
anuj bhatt <63633305+anujbhatt-dev@users.noreply.github.com>
2020-05-02 20:59:10 +0530

    Update game.js

--- game.js
+++ game.js
@@ -33,7 +33,7 @@ function checkAnswer(currentLevel) {
       setTimeout(function () {
         $("body").removeClass("game-over");
       }, 200);
-      $("#level-title").text("Game Over, Press Any Key to Restart");
+      $("#level-title").text("Game Over, Reload to Restart");
     }
 }
 function nextSequence() {
